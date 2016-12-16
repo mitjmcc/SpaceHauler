50128d3d1635221606faf0a11de098a4aec8f1bd
Mitch McClellan <mitchelljmcclellan@gmail.com>
2016-12-16 00:26:36 -0500

    Set up commit: ship, basic controls, tutorial scene, and project folders

---
+++ Assets/Scripts/TruckControlUtil.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Player Controller Utilities
+public class TCUtil {
+
+    public static Vector3 XZPlane = new Vector3(1, 0, 1);
+
+    public static void AdjustRigidbodyForward(Rigidbody body, Vector3 newForward, Vector3 camForward, float speed)
+    {
+        //Only rotate the body when there is motion
+        if (newForward.magnitude > 0)
+        {
+            //The direction of movement
+            Vector3 moveForward = new Vector3(newForward.x, 0, newForward.z).normalized, forward;
+            if (Vector3.Dot(moveForward, camForward) > 2)
+                //If the body is moving in the direction of the camera is pointing
+                forward = camForward;
+            else
+                //Or it is not going in the direction of the camera
+                forward = moveForward;
+            //Smooth the direction the body is facing to the correct direction
+            body.transform.forward = Vector3.Slerp(body.transform.forward, forward, speed * Time.fixedDeltaTime);
+        }
+    }
+
+    public static void HorizontalDrag(Rigidbody body, Vector3 velocity, float drag)
+    {
+        velocity.x *= 1 - drag;
+        velocity.y *= 1 - drag;
+        body.velocity = velocity;
+    }
+}
---
+++ Assets/Scripts/TruckController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using TeamUtility.IO;
+using System.Collections;
+
+public class TruckController : MonoBehaviour {
+
+    #region PublicVariables
+    public Vector2 lookXRestraints;
+    public Vector2 lookYRestraints;
+    public PlayerID player;
+
+    [Range(0f, 200f)]
+    public float moveForce;
+    [Range(0f, 5f)]
+    public float lookSpeed;
+    [Range(0f, 1f)]
+    public float drag = .1f;
+    #endregion
+
+    #region PrivateVariables
+    Rigidbody body;
+    Camera cam;
+    Transform model;
+    Animator anim;
+    Vector3 speed;
+    Vector3 forward;
+
+    float lookX;
+    float lookY;
+    float x, y, z;
+    #endregion
+
+    #region Initialization
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        cam = GetComponentInChildren<Camera>();
+        body = GetComponent<Rigidbody>();
+        model = transform.FindChild("Truck");
+    }
+    #endregion
+
+    #region Updates
+    void Update() {
+        //Get inputs from the camera inputs
+        lookX += InputManager.GetAxis("LookHorizontal", player) * lookSpeed;
+        lookY += InputManager.GetAxis("LookVertical", player) * lookSpeed;
+        //Limit the look axises
+        lookX = Mathf.Clamp(lookX, lookXRestraints.x, lookXRestraints.y);
+        lookY = Mathf.Clamp(lookY, lookYRestraints.x, lookYRestraints.y);
+        //Calculate the rotation using Euler angles,
+        Quaternion rotation = Quaternion.Euler(lookY, lookX, 0);
+        //Set the camera's new rotation
+        cam.transform.rotation = rotation;
+    }
+    #endregion
+
+    void FixedUpdate() {
+        //Get move inputs
+        x = InputManager.GetAxis("Vertical", player);
+        z = InputManager.GetAxis("Horizontal", player);
+        //Calculate the motion direction vector and scale it by the moveForce
+        speed = (x * body.transform.up + z * body.transform.right) * moveForce;
+        //Add the player's speed to the rigidbody velocity
+        body.velocity += speed;
+        //Drag
+        TCUtil.HorizontalDrag(body, body.velocity, drag);
+    }
+}
