eb36c428dba82ef2e08d3cb4be978c03c1b9746c
Mitch McClellan <mitchelljmcclellan@gmail.com>
2017-02-02 01:41:26 -0500

    Updated cockpit, collision, and win state

---
+++ Assets/CargoHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CargoHealth : MonoBehaviour {
+
+    //Might move this
+    public Image[] cargoImages;
+    public Text GameOver;
+
+    int cargo;
+
+	// Use this for initialization
+	void Start () {
+        cargo = cargoImages.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (cargo <= 0)
+        {
+            gameOver();
+        }
+	}
+
+    public void loseCargo()
+    {
+        if (cargo > 0)
+        {
+            cargoImages[cargo - 1].gameObject.SetActive(false);
+            cargo--;
+
+        }
+    }
+
+    public void gameOver()
+    {
+        GameOver.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}
