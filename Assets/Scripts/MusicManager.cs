796fd4304c1539c504dbe015f53f587c717bc5b0
kevin400085 <kevin400085@yahoo.com>
2017-02-16 00:14:03 -0500

    MusicManager
    Music Manager script now plays through given tracks and can have its
    volume adjusted through the settings manager

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour
+{
 
-    AudioClip music;
+    public AudioClip[] music;
+    public AudioSource musicSource;
+    public int track = 0;
+    public bool loopSingle;
 
-	void Start () {
-		
-	}
-	
-	void Update () {
-		
-	}
+    private Settings settings;
+
+    void Start()
+    {
+        track = track % music.Length;
+        musicSource.clip = music[track];
+        musicSource.Play();
+    }
+
+    void Update()
+    {
+        if (!musicSource.isPlaying)
+        {
+            if (!loopSingle)
+            {
+                track = (track + 1) % music.Length;
+            }
+            musicSource.clip = music[track];
+            musicSource.Play();
+        }
+        musicSource.volume = settings.volume;
+    }
+
 }
