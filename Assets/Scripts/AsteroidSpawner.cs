2d60701d4571ea2fea634022f35e0cd53b78efab
Mitch McClellan <mitchelljmcclellan@gmail.com>
2017-03-02 03:50:03 -0500

    Asteroids now spawn along the curve (if you change the curve, hit the regenerate button in the AsteroidSpawner), placeholder reticle, added scenes for the 5 levels, added Cargo Box script, added logo and font, Truck controller is fixed but jerky

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using YeggQuest.NS_Spline;
 
+[ExecuteInEditMode]
+[DisallowMultipleComponent]
 public class AsteroidSpawner : MonoBehaviour {
 
     [Header("Warp Tunnel Dimensions")]
@@ -8,7 +11,6 @@ public class AsteroidSpawner : MonoBehaviour {
     public float warpTunnelRadius = 50f;
     [Range(0f, 1f)]
     public float minimumAsteroidProbability;
-    public int length = 30;
     public int asteroidsPerSector = 10;
     public int sectors = 8;
     public int jitterRange = 10;
@@ -19,52 +21,93 @@ public class AsteroidSpawner : MonoBehaviour {
     public AnimationCurve distribution;
     public Material asteroidMaterial;
     public Mesh asteroidMesh;
+    public SplineWrapper wrapper;
 
     private ArrayList asteroidObjects;
     private GameObject asteroidParent;
+    float length;
 
-    // Use this for initialization
-    void Start() { 
-        spawnAsteroids(asteroidsPerSector);
+    void OnEnable()
+    {
+        if (wrapper == null)
+            wrapper = GameObject.FindObjectOfType<SplineMeshWrapper>();
     }
 
-    // Update is called once per frame
-    void Update() {
+    public void spawnAsteroids() {
+        if (Application.isPlaying)
+            return;
 
-    }
-
-    void spawnAsteroids(int max) {
-        asteroidParent = new GameObject("asteroidParent");
-        asteroidObjects = new ArrayList();
+        if (asteroidParent == null)
+            asteroidParent = new GameObject("asteroidParent" + Time.time);
+        //asteroidParent.transform.SetParent(transform.parent);
+        //asteroidObjects = new ArrayList();
         // Hand like a clock, it rotates
         Vector3 hand = new Vector3(0, 1, 0);
+        // Refernce to next vertex on the spline
+        SplineLerpResult vertex;
+        // Used for moving the asteroids around to remove uniformity
         Vector3 jitter;
         GameObject asteroidTemp, childTemp;
-        for (int k = 1; k < sectors; k++)
+        length = wrapper.spline.totalLength / wrapper.spline.vertices.Length;
+        // First loop: Iterate through the length of the tube
+        for (int j = 1; j < length; j++)
         {
-            for (int j = 1; j < length; j++)
+            // Second loop: Iterate through the sectors of a disk
+            for (int k = 1; k < sectors; k++)
             {
-                for (int i = 1; i < max; ++i)
+                // Third loop: Iterate through individual asteroids in a sector
+                for (int i = 1; i < asteroidsPerSector; ++i)
                 {
+                    // Calculate random jitter variation
                     jitter = Vector3.up * Random.Range(-jitterRange, jitterRange)
                         + Vector3.right * Random.Range(-jitterRange, jitterRange)
                         + Vector3.forward * Random.Range(-jitterRange, jitterRange);
+
+                    // Evaluate the curve to get a length along the sector
                     float coef = distribution.Evaluate(Random.value);
+
                     if (coef > 0) {
                         asteroidTemp = new GameObject("asteroid " + i);
-                        asteroidTemp.transform.position = hand * coef * warpTunnelRadius + jitter;
+                        
+                        asteroidTemp.transform.position = hand + coef * warpTunnelRadius * jitter;
                         asteroidTemp.transform.SetParent(asteroidParent.transform);
+
+                        // Spawn asteroid at random location
                         childTemp = (GameObject)GameObject.Instantiate(
                             asteroids[Random.Range(0, asteroids.Length)],
-                            hand * coef * warpTunnelRadius + jitter,
+                            asteroidTemp.transform.position,
                             Random.rotation);
+
                         childTemp.transform.SetParent(asteroidTemp.transform);
-                        asteroidObjects.Add(asteroidTemp);
+                        //asteroidObjects.Add(asteroidTemp);
                     }
                 }
-                hand = Quaternion.AngleAxis(360 / sectors, Vector3.forward) * hand;
+                // Rotate the hand
+                //hand = Quaternion.AngleAxis(360 / sectors, Vector3.forward) * hand;
             }
-            hand += Vector3.forward * lengthBetweenDisks;
+            // Move hand to the next location
+
+            vertex = wrapper.Lerp(new SplineLerpQuery(j / length));
+            hand += (vertex.worldPosition - hand);
         }
+        
     }
+
+    public void destroyAsteroids()
+    {
+        if (Application.isPlaying)
+            return;
+        if (asteroidParent == null)
+            return;
+
+        int temp = asteroidParent.transform.childCount;
+        while (temp != 0)
+        {
+            temp--;
+            DestroyImmediate(asteroidParent.transform.GetChild(0).gameObject);
+        }
+        DestroyImmediate(asteroidParent);
+        asteroidParent = null;
+    }
+
 }
